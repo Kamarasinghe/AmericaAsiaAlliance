5da2976dd8042b72ff0a3bcb750a16f7086d29ec
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-09-13 22:32:26 -0700

    Moved map information

---
+++ client/components/Map.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+
+const Map = withScriptjs(withGoogleMap(props => {
+  return (
+    <GoogleMap
+      defaultZoom={16}
+      defaultCenter={{ lat: 36.125113, lng: -115.190457 }}
+    >
+      <Marker
+        position={{ lat: 36.125113, lng: -115.190457 }}
+      />
+    </GoogleMap>
+  ); 
+}));
+
+export default Map;
