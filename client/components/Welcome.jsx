86c4ba22c2e1f1b46ee15fd12af921e6bfae15d8
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2018-09-17 21:17:44 -0700

    Added some styling to title and subtitle

--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 import {
   Card, CardImg, CardBody,CardTitle, 
   CardSubtitle, Row, Col
 } from 'reactstrap';
+
+const Title = styled.h1`
+  font-size: 3vw;
+  color: #A95D22;
+  padding-top: 20%;
+  padding-left: 15%;
+`;
+
+const SubTitle = styled.h4`
+  font-size: 1.5vw;
+  padding-left: 15%;
+  padding-top: 0.5%;
+`;
 
 const Welcome = () => {
   return (
@@ -11,8 +25,12 @@ const Welcome = () => {
         <CardBody>
           <Row>
             <Col xs="6">
-              <CardTitle>Welcome to America Asia Economic Alliance</CardTitle>
-              <CardSubtitle>Making our community a better place!</CardSubtitle>
+              <CardTitle>
+                <Title>Welcome to America Asia Economic Alliance</Title>
+              </CardTitle>
+              <CardSubtitle>
+                <SubTitle>Making our community a better place!</SubTitle>
+              </CardSubtitle>
             </Col>
             <Col xs="6">
               <CardImg top width="100%" src="https://previews.123rf.com/images/tonobalaguer/tonobalaguer1501/tonobalaguer150100194/35463827-manhattan-new-york-chamber-of-commerce-facade-and-columns-us.jpg" alt="Card image cap" />
